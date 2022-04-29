4e094dcc0c571ae9047fd62975d06df360e933fa
Alienxbe <martin.ykman@gmail.com>
2022-04-29 19:21:46 +0200

    Corrected flag `#`

--- srcs/types/ft_type_lx.c
+++ srcs/types/ft_type_lx.c
@@ -6,7 +6,7 @@
 /*   By: maykman <maykman@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 00:28:12 by maykman           #+#    #+#             */
-/*   Updated: 2022/04/29 19:15:55 by maykman          ###   ########.fr       */
+/*   Updated: 2022/04/29 19:21:15 by maykman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_type_lx(t_tag tag, va_list args)
 
 	n = va_arg(args, unsigned int);
 	s = ft_zutoa_base(n, BASE_HEXA_L);
-	if (!n  && tag.flags & FLAG_HASHTAG)
+	if (n  && tag.flags & FLAG_HASHTAG)
 		s = ft_addprefix(s, "0x");
 	return (ft_print(s, tag));
 }
